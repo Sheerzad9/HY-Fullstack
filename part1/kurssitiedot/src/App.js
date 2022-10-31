1c481340bd58edd5b2391ed5b45049b80a5643d0
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-10-31 19:03:34 +0200

    Part1 kurssitiedot tehtävä

---
+++ part1/kurssitiedot/src/App.js
@@ -0,0 +1,55 @@
+const Header = (props) => {
+  return (
+    <>
+      <h1>{props.course}</h1>
+    </>
+  );
+};
+
+const Part = (props) => {
+  return (
+    <>
+      <p>
+        {props.part.part} {props.part.exercises}
+      </p>
+    </>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <>
+      <Part part={props.data[0]} />
+      <Part part={props.data[1]} />
+      <Part part={props.data[2]} />
+    </>
+  );
+};
+
+const Total = (props) => {
+  let total = 0;
+  props.data.forEach((element) => {
+    total += element.exercises;
+  });
+
+  return <p>Number of exercises {total}</p>;
+};
+
+const App = () => {
+  const course = "Half Stack application development";
+  const data = [
+    { part: "Fundamentals of React", exercises: 10 },
+    { part: "Using props to pass data", exercises: 7 },
+    { part: "State of a component", exercises: 14 },
+  ];
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content data={data} />
+      <Total data={data} />
+    </div>
+  );
+};
+
+export default App;
