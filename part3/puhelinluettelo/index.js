375229b1ba666e1932740d941eaa6b207e302c18
Ali Sheerzad <ali.sheerzad9@gmail.com>
2022-11-13 15:52:46 +0200

    Tehtävät 3.7 & 3.8

--- part3/puhelinluettelo/index.js
+++ part3/puhelinluettelo/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 
 app.use(express.json());
+// Luodaan oma token tyyppi
+morgan.token("body", (req) => {
+  return JSON.stringify(req.body);
+});
+app.use(
+  morgan(":method :url :status :res[content-length] - :response-time ms :body")
+);
 
 let persons = [
   {
